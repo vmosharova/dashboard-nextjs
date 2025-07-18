55bb31674fb3b823f8d6b285d5d66992d6eea373
vmosharova <varya.mosharova@gmail.com>
2025-07-18 19:24:21 +0200

    Feat: revenue + recent invoices components; fetch data with postgres

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -43,6 +43,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    console.log('latestInvoices:', latestInvoices)
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
