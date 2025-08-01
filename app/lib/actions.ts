1265e4c511f076d40f77db967ce81b8dcca044c9
vmosharova <varya.mosharova@gmail.com>
2025-08-01 09:04:01 +0200

    Feat: server action (+ `.bind()`) to update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,7 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -31,6 +32,25 @@ export async function createInvoice(formData: FormData) {
   `;
 
   // new data submitted -> reouter cache should be revalidated:
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
