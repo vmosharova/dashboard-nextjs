51c67d7b55ae86238d474276e58069e0769e554c
vmosharova <varya.mosharova@gmail.com>
2025-08-08 11:19:57 +0200

    Feat: login + server action for login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 // This schema will validate the formData before saving it to a db:
 const FormSchema = z.object({
@@ -107,4 +109,24 @@ export async function deleteInvoice(id: string) {
   }
   
   revalidatePath('/dashboard/invoices');
+}
+
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
